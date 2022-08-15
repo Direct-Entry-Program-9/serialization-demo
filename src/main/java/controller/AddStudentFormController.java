b73c5a1623503cb2300a6598ffda55f9ac96c0f7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-08-15 16:58:40 +0530

    Finalize the UI

---
+++ src/main/java/controller/AddStudentFormController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class AddStudentFormController {
+    public JFXTextField txtStudentID;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentContact;
+    public JFXButton btnSaveStudent;
+
+    public void btnSaveStudentOnAction(ActionEvent actionEvent) {
+    }
+}
